d66ae3283546ecc4e706e4bc00c9ae1a2d26432d
Varghese <33537762+varghesepm@users.noreply.github.com>
2023-07-24 17:16:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,17 +6,23 @@ class RedisSentinelUser(User):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.sentinel = Sentinel([('int-redis-tester', 26379)], socket_timeout=0.1)
-        self.master = self.sentinel.master_for('int-redis-tester', socket_timeout=0.1)
+
+    def get_master(self):
+        return self.sentinel.discover_master('mymaster')
 
     @task(1)
     def write_to_redis(self):
         key = "test_key"
         value = "test_value"
+        master = self.get_master()
+        self.master = master[0]
         self.master.set(key, value)
 
     @task(2)
     def read_from_redis(self):
         key = "test_key"
+        master = self.get_master()
+        self.master = master[0]
         self.master.get(key)
 
 class RedisSentinelLocust(User):
