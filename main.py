f79388870e887cc7e1afa36aabc937039e2af712
Varghese <33537762+varghesepm@users.noreply.github.com>
2023-07-25 12:07:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-### Pyhton
-from kubernetes import client, config
 from locust import User, task, between
-import time 
 import redis
+from kubernetes import client, config
+import time
 
 class SentinelMasterPod:
-    def __init__(self, namespace='antelope-int', sentinel_service_name='int-redis-tester'):
+    def __init__(self, namespace='default', sentinel_service_name='int-redis-tester'):
         self.namespace = namespace
         self.sentinel_service_name = sentinel_service_name
         self.api = None
@@ -19,20 +18,33 @@ class SentinelMasterPod:
             raise RuntimeError("Failed to authenticate Kubernetes API: " + str(e))
 
     def get_sentinel_master_pod(self):
+        if not self.api:
+            self.authenticate_kubernetes_api()
+
         while True:
             try:
-                if not self.api:
-                    self.authenticate_kubernetes_api()
-
                 # Get the Service for the Redis Sentinel
                 service = self.api.read_namespaced_service(self.sentinel_service_name, self.namespace)
-                # Get the master pod name from the Service status
-                master_pod_name = service.status.load_balancer.ingress[0].hostname
-                return master_pod_name
+                # Check if the Service has an external IP or hostname
+                if service.spec.type == 'LoadBalancer' and service.status.load_balancer and service.status.load_balancer.ingress:
+                    if service.status.load_balancer.ingress[0].hostname:
+                        master_address = service.status.load_balancer.ingress[0].hostname
+                    else:
+                        master_address = service.status.load_balancer.ingress[0].ip
+                else:
+                    # Use the headless service name (StatefulSet format) to get the Redis master pod IP
+                    # Replace 'mymaster' with the actual Redis master pod name
+                    stateful_set_name = 'int-redis-tester-server'
+                    stateful_set_pod_list = self.api.list_namespaced_pod(self.namespace, label_selector=f"statefulset.kubernetes.io/pod-name={stateful_set_name}-0")
+                    if stateful_set_pod_list.items:
+                        master_address = stateful_set_pod_list.items[0].status.pod_ip
+                    else:
+                        raise ValueError("LoadBalancer hostname, ClusterIP, or ExternalIP not found in Service status.")
+                    
+                return master_address
             except Exception as e:
                 print(f"Error: {e}. Retrying in 5 seconds...")
                 time.sleep(5)
-
 
 class RedisSentinelUser(User):
     def __init__(self, *args, **kwargs):
@@ -42,21 +54,29 @@ class RedisSentinelUser(User):
 
     def on_start(self):
         # Initialize the SentinelMasterPod class with the namespace and the service name of the Redis Sentinel
-        sentinel_master = SentinelMasterPod(namespace='antelope-int', sentinel_service_name='int-redis-tester')
-        # Get the name of the Redis Sentinel master pod
+        sentinel_master = SentinelMasterPod(namespace='default', sentinel_service_name='int-redis-tester')
+        # Get the address of the Redis Sentinel master pod
         self.master_pod = sentinel_master.get_sentinel_master_pod()
-        self.sentinel_master = redis.StrictRedis(host=self.master_pod, port=26379, decode_responses=True)
+
+        # If master_pod is not found, raise an error or handle it as per your requirements
+        if self.master_pod is None:
+            raise ValueError("Redis Sentinel master pod address not found.")
+
+        self.sentinel_master = redis.StrictRedis(host=self.master_pod, port=6379, decode_responses=True)
 
     @task(1)
     def write_to_redis(self):
         key = "test_key"
         value = "test_value"
         self.sentinel_master.set(key, value)
+        print(f"Successfully set '{key}' to '{value}' in Redis.")
 
     @task(2)
     def read_from_redis(self):
-        key = "test_key"
-        self.sentinel_master.get(key)
+        # Fetch an existing key from Redis and use it with the GET command
+        existing_key = "test_key"
+        value = self.sentinel_master.get(existing_key)
+        print(f"Value of '{existing_key}': {value}")
 
 class RedisSentinelLocust(User):
     wait_time = between(1, 3)  # Time between consecutive tasks
